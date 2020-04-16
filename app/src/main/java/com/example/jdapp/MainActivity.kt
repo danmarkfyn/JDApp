82aedd0e319c277d86d0486b3602dff6a3255a4a
danmarkfyn <jakob2botg@gmail.com>
2020-04-16 16:56:10 +0200

    Connection with Firestore Cloud established

--- app/src/main/java/com/example/jdapp/MainActivity.kt
+++ app/src/main/java/com/example/jdapp/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.jdapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDB = FirebaseFirestore.getInstance()
+
+        val h1 = hashMapOf(
+            "name" to "Svendborg Sygehus",
+            "description" to "Hospital Located in Svendborg"
+            )
+        myDB.collection("Hospitals")
+            .add(h1 as Map<String, Any>)
+            .addOnSuccessListener { documentReference ->
+                println("DocumentSnapshot added with ID: ${documentReference.id}")
+
+            }
+            .addOnFailureListener { e ->
+                println("Error. Could not add document$e")
+            }
+
+
+
     }
+
+
 }
