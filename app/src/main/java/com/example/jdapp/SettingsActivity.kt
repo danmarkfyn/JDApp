51e75df61daf5e1b0ea5c11ba8a6400587d0c431
MetalFreak666 <egonoras@hotmail.com>
2020-04-21 16:01:16 +0200

    Implemented method for language change, not working

--- app/src/main/java/com/example/jdapp/SettingsActivity.kt
+++ app/src/main/java/com/example/jdapp/SettingsActivity.kt
@@ -2,21 +2,29 @@ package com.example.jdapp
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import io.opencensus.resource.Resource
 import java.util.*
 
 class SettingsActivity : AppCompatActivity() {
 
     lateinit var changeLanguageButton: Button
+    private var currentLanguage: String = "English"
+    private lateinit var myLocale: Locale
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        loadLocate()
 
         changeLanguageButton = findViewById(R.id.changeLanguageButton)
         changeLanguageButton.setOnClickListener{
@@ -24,8 +32,9 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     private fun languageOptions() {
-        //val langOptions: Array<> = resources.getStringArray(R.array.languageOptions)
+
         val langOptions = arrayOf("English", "Danish", "Polish")
         val aBuilder = AlertDialog.Builder(this)
         aBuilder.setTitle("Choose Language")
@@ -44,28 +53,33 @@ class SettingsActivity : AppCompatActivity() {
         }
         val aDialog = aBuilder.create()
         aDialog.show()
-    }
-
-    private fun setLanguage(Lang: String){
-        val locale = Locale(Lang)
-        Locale.setDefault(locale)
-        val config = Configuration()
-        config.locale = locale
-        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
-
-        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
-        editor.putString("My_Language", Lang)
-        editor.apply()
 
     }
 
-    private fun loadLocate() {
-        val sharedPreferecens = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
-        val language = sharedPreferecens.getString("My_Language", "")
-        setLanguage(language!!)
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private fun setLanguage(Lang: String) {
+        if(!Lang.equals(currentLanguage)) {
+
+            var context = applicationContext
+
+            val locale = Locale(Lang)
+            Locale.setDefault(locale)
+            val config = context.resources.configuration
+            setSystemLocale(config ,locale)
+
+
+            val refresh = Intent(this, SettingsActivity::class.java)
+            refresh.putExtra(currentLanguage, Lang)
+            startActivity(refresh)
+
+
+        } else {
+            Toast.makeText(this, "This is the current language ", Toast.LENGTH_LONG).show()
+        }
     }
 
-
-
-
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    fun setSystemLocale(config: Configuration, locale: Locale) {
+        config.setLocale(locale)
+    }
 }
